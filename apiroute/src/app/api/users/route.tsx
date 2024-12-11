572cd8605034a5bdd8dd45ab507dda89b827dd68
vatsal555 <prajapativatsal555@gmail.com>
2024-12-11 12:23:07 +0530

    post api route

--- apiroute/src/app/api/users/route.tsx
+++ apiroute/src/app/api/users/route.tsx
@@ -5,3 +5,20 @@ export function GET() {
   const data = users;
   return NextResponse.json(data, { status: 200 });
 }
+
+export async function POST(req: any) {
+  // console.log(req);
+
+  const payLoad = await req.json();
+  // console.log(payLoad);
+  if (!payLoad.name || !payLoad.age || !payLoad.email) {
+    return NextResponse.json(
+      { result: "require field not found", success: false },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { result: "new user created", success: true },
+    { status: 201 }
+  );
+}
