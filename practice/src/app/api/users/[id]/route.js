5e5ec7c9e8030b7a4ba11a31e6c39318e6518c33
vatsal555 <prajapativatsal555@gmail.com>
2024-12-11 19:08:04 +0530

    practice

---
+++ practice/src/app/api/users/[id]/route.js
@@ -0,0 +1,28 @@
+import { users } from "@/util/db";
+import { NextResponse } from "next/server";
+
+export function GET(req, res) {
+  console.log(res);
+
+  const userData = users.filter((item) => item.id === res.params.id);
+  return NextResponse.json(
+    userData.length == 0
+      ? { result: "No Data Found", success: false }
+      : { result: userData[0], success: true },
+    { status: 200 }
+  );
+}
+
+export async function POST(req) {
+  const payLoad = await req.json();
+  // console.log(payLoad);
+  if (!payLoad.id || !payLoad.name || !payLoad.email || !payLoad.age) {
+    return NextResponse.json(
+      { result: "request data is missing", success: false },
+      { status: 400 }
+    );
+  } else {
+  }
+
+  return NextResponse.json({ result: payLoad, success: true }, { status: 200 });
+}
---
+++ practice/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Practice Route with Next Js</h1>
+    </div>
+  );
+}
