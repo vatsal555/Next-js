ef2945f3d9d49514a8166d62c86f8bdc074106b2
vatsal555 <prajapativatsal555@gmail.com>
2024-12-16 11:25:40 +0530

    put method

--- next-api/src/app/api/products/[productid]/route.tsx
+++ next-api/src/app/api/products/[productid]/route.tsx
@@ -1,10 +1,17 @@
+import { mongoDBConnect } from "@/lib/db";
+import { Product } from "@/lib/model/product";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function PUT(req, content) {
-  // console.log(content?.params);
-  const filter = content.params.productid;
+  // console.log(content);
+  const productid = content.params.productid;
+  const filter = { _id: productid };
+  const payLoad = await req.json();
+  await mongoose.connect(mongoDBConnect);
+  const result = await Product.findOneAndUpdate(filter, payLoad);
   //   console.log(filter);
 
-  const result: any = [];
+  // const result: any = [];
   return NextResponse.json({ result, success: true });
 }
